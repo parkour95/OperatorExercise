faca4e6cf1dcc26e678b0403157da2f4f489d948
Parker Richmond <prjax95@gmail.com>
2021-10-22 18:31:29 -0400

    Add project files.

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int subtract = a - b;
+            int product = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a} + {b} is {sum}");
+            Console.WriteLine($"{a} - {b} is {subtract}");
+            Console.WriteLine($"{a} * {b} is {product}");
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+        }
+    }
+}
